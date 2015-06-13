e064866273e65b52e7ced471aeca745c192d76e6
瀬崎正太郎 <09hn054s@gmail.com>
2015-06-13 18:08:00 +0900

    新規登録機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,4 +10,29 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    #formから投げられたデータを受け取る
+    @item = Item.new(item_params)
+
+    #これを保存する
+    @item.save
+
+    #show.html.erbに飛ばす
+    redirect_to "/items/#{@item.id}"
+  end
+
+  private
+  def item_params
+    #params.require(:key).permit(:filter)
+    params.require(:item).permit(
+      :name,
+      :description,
+      :seller_id,
+      :price,
+      :email,
+      :image_url
+      )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'items/new' => 'items#new'
   get '/items/' => 'items#index'
   get 'items/:id' => 'items#show'
+  post '/items' => 'items#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
